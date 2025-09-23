595beea486ca0d5561420279b1d1ef343c054e13
ErwinPetit <erwin@playmoweb.com>
2025-09-23 09:24:04 +0200

    feat: init DatabaseSessionService on test-agent

--- python/agents/blog-writer/blogger_agent/config.py
+++ python/agents/blog-writer/blogger_agent/config.py
@@ -19,13 +19,13 @@ import google.auth
 
 # To use AI Studio credentials:
 # 1. Create a .env file in the /app directory with:
-#    GOOGLE_GENAI_USE_VERTEXAI=FALSE
-#    GOOGLE_API_KEY=PASTE_YOUR_ACTUAL_API_KEY_HERE
+#GOOGLE_GENAI_USE_VERTEXAI=FALSE
+#GOOGLE_API_KEY=PASTE_YOUR_ACTUAL_API_KEY_HERE
 # 2. This will override the default Vertex AI configuration
-_, project_id = google.auth.default()
-os.environ.setdefault("GOOGLE_CLOUD_PROJECT", project_id)
-os.environ.setdefault("GOOGLE_CLOUD_LOCATION", "global")
-os.environ.setdefault("GOOGLE_GENAI_USE_VERTEXAI", "True")
+#_, project_id = google.auth.default()
+#os.environ.setdefault("GOOGLE_CLOUD_PROJECT", project_id)
+#os.environ.setdefault("GOOGLE_CLOUD_LOCATION", "global")
+#os.environ.setdefault("GOOGLE_GENAI_USE_VERTEXAI", "True")
 
 
 @dataclass
@@ -41,6 +41,6 @@ class ResearchConfiguration:
     critic_model: str = "gemini-2.5-pro"
     worker_model: str = "gemini-2.5-flash"
     max_search_iterations: int = 5
-
+    database_url: str = os.getenv("DATABASE_URL", "postgresql+psycopg://cointreau:cointreaupassword@localhost:5432/cointreau")
 
 config = ResearchConfiguration()
--- python/agents/blog-writer/tests/test_agent.py
+++ python/agents/blog-writer/tests/test_agent.py
@@ -1,35 +1,49 @@
 import asyncio
 
 from google.adk.runners import Runner
-from google.adk.sessions import InMemorySessionService
 from blogger_agent.agent import root_agent
 from google.genai import types as genai_types
+from google.adk.sessions import DatabaseSessionService
+from blogger_agent.config import config
 
 
 async def main():
+
     """Runs the agent with a sample query."""
-    session_service = InMemorySessionService()
-    await session_service.create_session(
-        app_name="app", user_id="test_user", session_id="test_session"
+    session_service = DatabaseSessionService(db_url=config.database_url)
+
+    session_id="session-erwin-3"
+    user_id="user-erwin"
+
+    existing_session = await session_service.get_session(
+        app_name="app",
+        user_id=user_id,
+        session_id=session_id,
     )
+
+    if existing_session:
+        print("Existing sessions:")
+        print(existing_session)
+    else:
+        await session_service.create_session(
+            app_name="app", user_id=user_id, session_id=session_id
+        )
+
+
     runner = Runner(
         agent=root_agent, app_name="app", session_service=session_service
     )
 
+
     queries = [
-        "I want to write a blog post about the new features in the latest version of the ADK.",
-        "looks good, write it",
-        "1",
-        "looks good, I approve",
-        "yes",
-        "my_new_blog_post.md",
+        "I want to understand seagulls.",
     ]
 
     for query in queries:
         print(f">>> {query}")
         async for event in runner.run_async(
-            user_id="test_user",
-            session_id="test_session",
+            user_id=user_id,
+            session_id=session_id,
             new_message=genai_types.Content(
                 role="user", 
                 parts=[genai_types.Part.from_text(text=query)]
